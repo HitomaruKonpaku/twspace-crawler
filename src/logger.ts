a1fc6227674a0a7424c3615cfc669288f69b6a8a
HitomaruKonpaku <hunteratthenight@gmail.com>
2021-10-16 00:51:21 +0700

    Update logger

--- src/logger.ts
+++ src/logger.ts
@@ -2,41 +2,48 @@ import winston, { format } from 'winston'
 import DailyRotateFile from 'winston-daily-rotate-file'
 import { config } from './config'
 
-function getConsoleFormat() {
-  return format.printf((info) => {
-    const date = new Date().toISOString()
-    const msg = [
-      date,
-      info.level,
-      typeof info.message === 'string' ? info.message : JSON.stringify(info.message),
-    ].filter((v) => v).join(' ')
-    return msg
-  })
-}
-
-function getFileFormat() {
-  return getConsoleFormat()
+function getPrintFormat() {
+  return format.printf((info) => [
+    info.timestamp,
+    info.level,
+    info.label,
+    typeof info.message === 'string' ? info.message : JSON.stringify(info.message),
+    Object.keys(info.metadata).length ? JSON.stringify(info.metadata) : '',
+  ].filter((v) => v).join(' '))
 }
 
 const logger = winston.createLogger({
   format: format.combine(
+    format.metadata(),
+    format.timestamp(),
     format((info) => Object.assign(info, { level: info.level.toUpperCase() }))(),
+    format((info) => {
+      const { metadata } = info
+      if (metadata.label) {
+        Object.assign(info, { label: metadata.label })
+        delete metadata.label
+      }
+      return info
+    })(),
   ),
   transports: [
     new winston.transports.Console({
       level: 'verbose',
-      format: format.combine(format.colorize(), getConsoleFormat()),
+      format: format.combine(
+        format.colorize(),
+        getPrintFormat(),
+      ),
     }),
     new DailyRotateFile({
       level: 'verbose',
-      format: format.combine(getFileFormat()),
+      format: format.combine(getPrintFormat()),
       datePattern: config.logger.datePattern,
       dirname: config.logger.dir,
       filename: '%DATE%.log',
     }),
     new DailyRotateFile({
       level: 'silly',
-      format: format.combine(getFileFormat()),
+      format: format.combine(getPrintFormat()),
       datePattern: config.logger.datePattern,
       dirname: config.logger.dir,
       filename: '%DATE%_all.log',
