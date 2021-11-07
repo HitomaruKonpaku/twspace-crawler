6de27afb5919f593fae106380e7713f19fd0626e
HitomaruKonpaku <hunteratthenight@gmail.com>
2021-11-07 22:47:18 +0700

    Reduce default user refresh interval

--- src/constants/app.constant.ts
+++ src/constants/app.constant.ts
@@ -1,4 +1,4 @@
 export const APP_CACHE_DIR = '../.cache'
 export const APP_MEDIA_DIR = '../.media'
-export const APP_USER_REFRESH_INTERVAL = 10000
+export const APP_USER_REFRESH_INTERVAL = 30000
 export const APP_PLAYLIST_REFRESH_INTERVAL = 10000
