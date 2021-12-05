d4ea3a70f2989f14092a8b7de02c982dba672a2f
HitomaruKonpaku <hunteratthenight@gmail.com>
2021-12-05 21:52:38 +0700

    Update captions download chunk should retry in some case

--- src/SpaceCaptionsDownloader.ts
+++ src/SpaceCaptionsDownloader.ts
@@ -29,16 +29,26 @@ export class SpaceCaptionsDownloader {
       this.logger.info(`Downloading captions to ${this.file}`)
       fs.writeFileSync(this.file, '')
       do {
-        this.logger.info(`Downloading chunk ${this.chunkCount}`)
-        // eslint-disable-next-line no-await-in-loop
-        const history = await this.getChatHistory()
-        const { messages } = history
-        messages.forEach((message) => {
-          fs.appendFileSync(this.file, `${JSON.stringify(message)}\n`)
-        })
-        this.chunkCount += 1
-        this.cursor = history.cursor
-      } while (this.cursor)
+        try {
+          this.logger.info(`Downloading chunk ${this.chunkCount}`)
+          this.logger.debug(`Current cursor: "${this.cursor}"`)
+          // eslint-disable-next-line no-await-in-loop
+          const history = await this.getChatHistory()
+          const { messages } = history
+          messages.forEach((message) => {
+            fs.appendFileSync(this.file, `${JSON.stringify(message)}\n`)
+          })
+          this.chunkCount += 1
+          this.cursor = history.cursor
+          this.logger.debug(`Next cursor: "${this.cursor}"`)
+        } catch (error) {
+          const msg = error.message
+          if (!['socket hang up', 'connect ETIMEDOUT'].some((v) => msg.includes(v))) {
+            throw error
+          }
+          this.logger.error(msg)
+        }
+      } while (this.cursor || this.chunkCount <= 1)
     } catch (error) {
       this.logger.error(error.message)
     }
