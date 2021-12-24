d2d13ac82649d1fd0c9bb7a122292bd398c96c2e
HitomaruKonpaku <hunteratthenight@gmail.com>
2021-12-24 13:52:27 +0700

    Update UserWatcher should use public Twitter API instead

---
+++ src/TwitterApi.ts
@@ -0,0 +1,76 @@
+import axios from 'axios'
+
+export class TwitterApi {
+  public static async getGuestToken(): Promise<string> {
+    const { data } = await axios.get<string>('https://twitter.com/')
+    const token = /(?<=gt=)\d{19}/.exec(data)[0]
+    return token
+  }
+
+  public static async getSpacesByCreatorIds(ids: string[], headers: Record<string, string>) {
+    const { data } = await axios.get('https://api.twitter.com/2/spaces/by/creator_ids', {
+      headers,
+      params: { user_ids: ids.join(',') },
+    })
+    return data
+  }
+
+  public static async getUserByScreenName(username: string, headers: Record<string, string>) {
+    const { data } = await axios.get('https://twitter.com/i/api/graphql/7mjxD3-C6BxitPMVQ6w0-Q/UserByScreenName', {
+      headers,
+      params: {
+        variables: {
+          screen_name: username,
+          withSafetyModeUserFields: false,
+          withSuperFollowsUserFields: false,
+        },
+      },
+    })
+    return data
+  }
+
+  public static async getUserTweets(userId: string, headers: Record<string, string>) {
+    const { data } = await axios.get('https://twitter.com/i/api/graphql/QvCV3AU7X1ZXr9JSrH9EOA/UserTweets', {
+      headers,
+      params: {
+        variables: {
+          userId,
+          count: 10,
+          withTweetQuoteCount: false,
+          includePromotedContent: false,
+          withQuickPromoteEligibilityTweetFields: false,
+          withSuperFollowsUserFields: false,
+          withBirdwatchPivots: false,
+          withDownvotePerspective: false,
+          withReactionsMetadata: false,
+          withReactionsPerspective: false,
+          withSuperFollowsTweetFields: false,
+          withVoice: false,
+          withV2Timeline: false,
+        },
+      },
+    })
+    return data
+  }
+
+  public static async getAudioSpaceById(spaceId: string, headers: Record<string, string>) {
+    const { data } = await axios.get('https://twitter.com/i/api/graphql/Uv5R_-Chxbn1FEkyUkSW2w/AudioSpaceById', {
+      headers,
+      params: {
+        variables: {
+          id: spaceId,
+          isMetatagsQuery: false,
+          withSuperFollowsUserFields: false,
+          withBirdwatchPivots: false,
+          withDownvotePerspective: false,
+          withReactionsMetadata: false,
+          withReactionsPerspective: false,
+          withSuperFollowsTweetFields: false,
+          withReplays: false,
+          withScheduledSpaces: false,
+        },
+      },
+    })
+    return data
+  }
+}
