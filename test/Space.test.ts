43fbdb0bba4d567acb10cb0503c91903db288d38
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-04-24 23:11:59 +0700

    Update test

--- test/Space.test.ts
+++ test/Space.test.ts
@@ -8,25 +8,25 @@ const lab = Lab.script()
 const { describe, it } = lab
 export { lab }
 
+// const spaces = Array
+//   // eslint-disable-next-line no-eval
+//   .from<any>(eval(readFileSync(path.join(__dirname, 'data/spaces.ts'), { encoding: 'utf8' })))
+//   .filter((space) => space.playlist_url)
+
 const spaces = Array
-  // eslint-disable-next-line no-eval
-  .from<any>(eval(readFileSync(path.join(__dirname, 'data/spaces.ts'), { encoding: 'utf8' })))
-  .filter((space) => space.playlist_url)
+  .from<any>(readFileSync(path.join(__dirname, '../logs/spaces.jsonl'), { encoding: 'utf8' }).trim().split('\n'))
+  .filter((v) => v)
+  .map((v) => JSON.parse(v))
 
 describe('Space', () => {
-  // describe('Playlist Duplication', () => {
-  //   const set = new Set()
-  //   spaces.forEach((space) => {
-  //     it(`@${space.username} (${space.id})`, () => {
-  //       expect(set.has(space.playlist_url)).false()
-  //       set.add(space.playlist_url)
-  //     })
-  //   })
-  // })
-
   describe('Playlist Status', () => {
+    const ids = new Set<string>()
     spaces.forEach((space) => {
-      it(`@${space.username} (${space.id})`, async () => {
+      if (ids.has(space.id)) {
+        return
+      }
+      ids.add(space.id)
+      it(`@${space.username} (${space.id}) [${new Date(space.started_at).toISOString()}]`, async () => {
         const { status } = await axios.head(space.playlist_url)
         expect(status).equal(200)
       })
