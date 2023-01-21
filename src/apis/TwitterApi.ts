76b3282ca2e351b9b9cb2dca4132342c7cccf15b
HitomaruKonpaku <hunteratthenight@gmail.com>
2023-01-22 01:18:07 +0700

    Update AudioSpace endpoint

--- src/apis/TwitterApi.ts
+++ src/apis/TwitterApi.ts
@@ -101,20 +101,39 @@ export class TwitterApi {
   }
 
   public static async getAudioSpaceById(spaceId: string, headers: Record<string, string>) {
-    const { data } = await axios.get('https://twitter.com/i/api/graphql/Uv5R_-Chxbn1FEkyUkSW2w/AudioSpaceById', {
+    const url = 'https://api.twitter.com/graphql/xjTKygiBMpX44KU8ywLohQ/AudioSpaceById'
+    const { data } = await axios.get(url, {
       headers,
       params: {
         variables: {
           id: spaceId,
-          isMetatagsQuery: false,
-          withSuperFollowsUserFields: false,
-          withBirdwatchPivots: false,
+          isMetatagsQuery: true,
+          withSuperFollowsUserFields: true,
           withDownvotePerspective: false,
           withReactionsMetadata: false,
           withReactionsPerspective: false,
-          withSuperFollowsTweetFields: false,
-          withReplays: false,
-          withScheduledSpaces: false,
+          withSuperFollowsTweetFields: true,
+          withReplays: true,
+        },
+        features: {
+          spaces_2022_h2_clipping: true,
+          spaces_2022_h2_spaces_communities: true,
+          responsive_web_twitter_blue_verified_badge_is_enabled: true,
+          verified_phone_label_enabled: false,
+          view_counts_public_visibility_enabled: true,
+          longform_notetweets_consumption_enabled: false,
+          tweetypie_unmention_optimization_enabled: true,
+          responsive_web_uc_gql_enabled: true,
+          vibe_api_enabled: true,
+          responsive_web_edit_tweet_api_enabled: true,
+          graphql_is_translatable_rweb_tweet_is_translatable_enabled: true,
+          view_counts_everywhere_api_enabled: true,
+          standardized_nudges_misinfo: true,
+          tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled: false,
+          responsive_web_graphql_timeline_navigation_enabled: true,
+          interactive_text_enabled: true,
+          responsive_web_text_conversations_enabled: false,
+          responsive_web_enhance_cards_enabled: false,
         },
       },
     })
